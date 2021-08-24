a7f3b7118311e85f059a27abfd4edcdbc5b0e886
Sid Dhulipalla <sid.dhulipalla@databricks.com>
2021-08-23 21:21:32 -0700

    add photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import './App.css';
+import shivani3 from "./photos/3.jpg";
+import shivani4 from "./photos/4.jpg";
+import shivani5 from "./photos/5.jpg";
+import shivani11 from "./photos/11.jpg";
+import shivani13 from "./photos/13.jpg";
+import shivani14 from "./photos/14.jpg";
+import shivani15 from "./photos/15.jpg";
+import shivani16 from "./photos/16.jpg";
+import shivani17 from "./photos/17.jpg";
+import shivani18 from "./photos/18.jpg";
+import shivani19 from "./photos/19.jpg";
+import shivani20 from "./photos/20.jpg";
 
 interface IProps {}
 interface IState {
@@ -44,8 +56,7 @@ class App extends React.Component<IProps, IState> {
     delta -= minutes * 60;
     var seconds = Math.floor(delta % 60);
 
-    var text = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
-    return text;
+    return {days, hours, minutes, seconds};
   }
 
   isToday(someDate : Date) {
@@ -55,22 +66,66 @@ class App extends React.Component<IProps, IState> {
   }    
 
   render() {
-    var text = this.getDateDiff();
+    var timeLeft = this.getDateDiff();
+    var text;
     if (this.isToday(this.birthday)) {
-      text = "It's TODAY!! Happy Birthday Shivani!!"
+      text = <p>"It's TODAY!! Happy Birthday Shivani!!"</p>
+    } else {
+      text = (
+      <div>
+          <p> {timeLeft.days} days</p>
+          <p> {timeLeft.hours} hours</p>
+          <p> {timeLeft.minutes} minutes</p>
+          <p> {timeLeft.seconds} seconds</p>
+        </div>
+      )
     }
+
+
     return (
       <div className="App">
         <header className="App-header">
           <p>
             Countdown to Shivani's Birthday
           </p>
-          <p>
-            {text}
-          </p>
+          {text}
         </header>
+        <PhotoViewer />
       </div>
     );
+  }
+}
+
+interface IPhotoState {
+  index : number;
+}
+
+class PhotoViewer extends React.Component<{}, IPhotoState>  {
+
+  interval : any;
+  photos = [shivani3,shivani4,shivani5,shivani11,shivani13,shivani14,shivani15,shivani16,shivani17,shivani18,shivani19,shivani20];
+
+  constructor(props : any) {
+    super(props);
+    this.state = {
+      index: 0
+    };
+  }
+
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ index: (this.state.index + 1) % this.photos.length }), 5000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="PhotoViewer">
+       <img src={this.photos[this.state.index]} />
+      </div>
+    )
   }
 }
 
